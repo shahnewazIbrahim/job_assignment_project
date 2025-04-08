8d9ef66e0e4fad92159d142d0993ce73165dca5f
shahnewazIbrahim <shahnewaz886@gmail.com>
2025-04-08 13:21:11 +0600

    Reintroduce featured products section in home view with updated layout and structure for improved maintainability

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -98,7 +98,30 @@
         <!-- END SECTION SINGLE BANNER -->
 
         <!-- START SECTION SHOP -->
-        @include('components.featured_products', compact('featured_products'))
+        <div class="section">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-6">
+                        <div class="heading_s1 text-center">
+                            <h2>Featured Products</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="product_slider carousel_slider owl-carousel owl-theme nav_style1" data-loop="true"
+                            data-dots="false" data-nav="true" data-margin="20"
+                            data-responsive='{"0":{"items": "1"}, "481":{"items": "2"}, "768":{"items": "3"}, "1199":{"items": "4"}}'>
+                            @foreach ($featured_products as $product)
+                                <div class="item">
+                                    @include('components.product_card', compact('product'))
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <!-- END SECTION SHOP -->
 
         <!-- START SECTION TESTIMONIAL -->
